1dcbfe2356d58734f95e6a8fecd292364bc42722
Xiaopu Zhang <79245625+ZXiaopu@users.noreply.github.com>
2024-05-16 09:40:32 +0100

    Update 03c.post_osca_rename_alleles.R

--- Scripts/03c.post_osca_rename_alleles.R
+++ Scripts/03c.post_osca_rename_alleles.R
@@ -7,13 +7,14 @@ trait <- args[1]
 
 process <- function(method){
     vGWAS <- rbind(read_delim(paste0("/mnt/project/DataAnalysis/05.OSCA/", trait, "/", trait, "_", method, "_arrayedSNPs")),
-                   read_delim(paste0("/mnt/project/DataAnalysis/05.OSCA/", trait, "/", trait, "_", method, "_imputedSNPs"))) %>%
+                   read_delim(paste0("/mnt/project/DataAnalysis/05.OSCA/", trait, "/", trait, "_", method, "_genotypedSNPs"))) %>%
         mutate(genotype1=paste0(Chr,":",BP,"_",A1,"_",A2)) %>%
         mutate(genotype2=paste0(Chr,":",BP,"_",A2,"_",A1))
-    snplist <- read_delim(paste0("/mnt/project/DataAnalysis/04.Regenie/",trait,"/",trait,".snplist"), col_names=F, delim="\t")
+    snplist <- rbind(read_delim("ukb22418_imputed_postQC_EUR_MAF0.05_HWE1e-6_INFO.8.recode.snplist"), col_names=F, delim="\t"),
+                     read_delim("ukb22418_genotyped_postQC_EUR_MAF0.05_HWE1e-6_GENO0.05.recode.snplist"), col_names=F, delim="\t"))
 
-    vGWAS.geno1 <- vGWAS %>% filter(genotype1 %in% snplist$X1) %>% mutate(ID=genotype1) %>% select(-genotype1, -genotype2)
-    vGWAS.geno2 <- vGWAS %>% filter(genotype2 %in% snplist$X1) %>% mutate(ID=genotype2) %>% select(-genotype1, -genotype2)
+    vGWAS.geno1 <- vGWAS %>% filter(genotype1 %in% snplist$X2) %>% mutate(ID=genotype1) %>% select(-genotype1, -genotype2)
+    vGWAS.geno2 <- vGWAS %>% filter(genotype2 %in% snplist$X2) %>% mutate(ID=genotype2) %>% select(-genotype1, -genotype2)
 
     vGWASres <- rbind(vGWAS.geno1, vGWAS.geno2)
     
